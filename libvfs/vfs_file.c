8bd43e866782f1fd9c83fb80b77fa65636936b61
abricoshka <144444105+abricoshka@users.noreply.github.com>
2026-07-02 21:30:19 +0700

    Update vfs_file.c

--- libvfs/vfs_file.c
+++ libvfs/vfs_file.c
@@ -1,8 +1,17 @@
+#include <errno.h>
 #include <fcntl.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+
+#ifdef _WIN32
+#include <io.h>
+#else
 #include <unistd.h>
+#endif
+
 #include "vfs.h"
 #include "vfs_internal.h"
 
@@ -15,20 +24,33 @@ static int
 file_fsync(FHANDLE fd_)
 {
     struct file_ops_file *fd = (struct file_ops_file *)fd_;
+
     if (!fd) {
         return -1;
     }
+
+#ifdef _WIN32
+    return _commit(fd->fd);
+#else
     return fsync(fd->fd);
+#endif
 }
 
 static int
 file_close(FHANDLE fd_)
 {
     struct file_ops_file *fd = (struct file_ops_file *)fd_;
+
     if (!fd) {
         return -1;
     }
+
+#ifdef _WIN32
+    _close(fd->fd);
+#else
     close(fd->fd);
+#endif
+
     free(fd);
     return 0;
 }
@@ -37,39 +59,60 @@ static ssize_t
 file_read(FHANDLE fd_, void *buf, size_t count)
 {
     struct file_ops_file *fd = (struct file_ops_file *)fd_;
+
     if (!fd) {
         return -1;
     }
+
+#ifdef _WIN32
+    return (ssize_t)_read(fd->fd, buf, (unsigned int)count);
+#else
     return read(fd->fd, buf, count);
+#endif
 }
 
 static ssize_t
 file_write(FHANDLE fd_, const void *buf, size_t count)
 {
     struct file_ops_file *fd = (struct file_ops_file *)fd_;
+
     if (!fd) {
         return -1;
     }
+
+#ifdef _WIN32
+    return (ssize_t)_write(fd->fd, buf, (unsigned int)count);
+#else
     return write(fd->fd, buf, count);
+#endif
 }
 
 static off_t
 file_lseek(FHANDLE fd_, off_t offset, int whence)
 {
     struct file_ops_file *fd = (struct file_ops_file *)fd_;
+
     if (!fd) {
         return -1;
     }
+
+#ifdef _WIN32
+    return (off_t)_lseeki64(fd->fd, (__int64)offset, whence);
+#else
     return lseek(fd->fd, offset, whence);
+#endif
 }
 
 static int
 file_ioctl(FHANDLE fd_, unsigned long req, ...)
 {
     struct file_ops_file *fd = (struct file_ops_file *)fd_;
+
     if (!fd) {
         return -1;
     }
+
+    (void)req;
     return -1;
 }
 
@@ -77,26 +120,44 @@ static int
 file_ftruncate(FHANDLE fd_, off_t length)
 {
     struct file_ops_file *fd = (struct file_ops_file *)fd_;
+
     if (!fd) {
         return -1;
     }
+
+#ifdef _WIN32
+    return _chsize_s(fd->fd, (__int64)length) == 0 ? 0 : -1;
+#else
     return ftruncate(fd->fd, length);
+#endif
 }
 
 static ssize_t
 file_length(FHANDLE fd_)
 {
     struct file_ops_file *fd = (struct file_ops_file *)fd_;
-    int rv;
-    struct stat st;
+
     if (!fd) {
         return -1;
     }
-    rv = fstat(fd->fd, &st);
-    if (rv) {
+
+#ifdef _WIN32
+    struct _stati64 st;
+
+    if (_fstati64(fd->fd, &st) != 0) {
         return -1;
     }
-    return st.st_size;
+
+    return (ssize_t)st.st_size;
+#else
+    struct stat st;
+
+    if (fstat(fd->fd, &st) != 0) {
+        return -1;
+    }
+
+    return (ssize_t)st.st_size;
+#endif
 }
 
 FHANDLE
@@ -104,21 +165,39 @@ file_open(const char *pathname, int flags, ...)
 {
     mode_t mode = 0;
     struct file_ops_file *ops;
+
     ops = malloc(sizeof(*ops));
     if (!ops) {
         return NULL;
     }
+
     if (flags & O_CREAT) {
         va_list ap;
+
         va_start(ap, flags);
-        mode = va_arg(ap, int);
+        mode = (mode_t)va_arg(ap, int);
         va_end(ap);
     }
+
+#ifdef _WIN32
+    ops->fd = _open(pathname, flags | _O_BINARY, mode);
+#else
     ops->fd = open(pathname, flags, mode);
+#endif
+
     if (ops->fd < 0) {
+        fprintf(
+            stderr,
+            "[e] cannot open '%s': %s (errno=%d)\n",
+            pathname,
+            strerror(errno),
+            errno
+        );
+
         free(ops);
         return NULL;
     }
+
     ops->ops.flags = flags & O_ACCMODE;
     ops->ops.read = file_read;
     ops->ops.write = file_write;
@@ -128,5 +207,6 @@ file_open(const char *pathname, int flags, ...)
     ops->ops.fsync = file_fsync;
     ops->ops.close = file_close;
     ops->ops.length = file_length;
+
     return (FHANDLE)ops;
 }
